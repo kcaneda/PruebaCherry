a059938a559132f1b2fbb177903ec35496768c4b
ricardddcode <ricardoreyengasado@gmail.com>
2026-05-13 08:02:50 -0600

    Commit mio

---
+++ Cherry/src/main/java/com/kennethcaneda/Cherry/CherryApplication.java
@@ -0,0 +1,13 @@
+package com.kennethcaneda.Cherry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherryApplication.class, args);
+	}
+
+}
